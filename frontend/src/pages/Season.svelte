aa52ec7c1e2d27b0d032fc779ab794a89dfd4c2f
Corydon Baylor <cwbaylor1@gmail.com>
2021-12-12 17:20:58 -0500

    welp

--- frontend/src/pages/Season.svelte
+++ frontend/src/pages/Season.svelte
@@ -1,10 +1,4 @@
 <script>
-  import { onMount } from "svelte";
-
-  onMount(async () => {
-    const res = await fetch(`./location`);
-    location = await res.json();
-  });
 </script>
 
 <div>test</div>
