8241c922ea6aad555c2fda5e271b22edee2f6e58
Corydon Baylor <cwbaylor1@gmail.com>
2021-12-12 13:59:42 -0500

    first attempt at getting location

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_from_directory
+from flask import request
 import random
 
 app = Flask(__name__)
@@ -17,6 +18,17 @@ def home(path):
 @app.route("/rand")
 def hello():
     return str(random.randint(0, 100))
+
+@app.route("/location")
+def get_country():
+    ip_address = request.remote_addr
+    try:
+        response = requests.get("http://ip-api.com/json/{}".format(ip_address))
+        js = response.json()
+        country = js['countryCode']
+        return country
+    except Exception as e:
+        return ip_address
 
 # Send 404 errors to index and let front end handle routing
 @app.errorhandler(404)   
