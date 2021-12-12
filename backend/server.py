230b02f999f534777a61c4e5de51a5ff7306393e
Corydon Baylor <cwbaylor1@gmail.com>
2021-12-12 14:12:58 -0500

    reverse proxy

--- backend/server.py
+++ backend/server.py
@@ -21,13 +21,17 @@ def hello():
 
 @app.route("/location")
 def get_country():
+
+    headers_list = request.headers.getlist("X-Forwarded-For")
+    user_ip = headers_list[0] if headers_list else request.remote_addr
+
     try:
-        response = requests.get("http://ip-api.com/json/{}".format(request.remote_addr))
+        response = requests.get("http://ip-api.com/json/{}".format(user_ip))
         js = response.json()
         country = js['countryCode']
         return country
     except Exception as e:
-        return str(request.remote_addr)
+        return str(user_ip)
 
 # Send 404 errors to index and let front end handle routing
 @app.errorhandler(404)   
