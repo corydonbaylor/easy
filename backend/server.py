585b438be843e326bb53db7b1205fce3cbe932bf
Corydon Baylor <cwbaylor1@gmail.com>
2021-12-12 16:36:05 -0500

    json with date and location

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,8 @@
 from flask import Flask, send_from_directory
 from flask import request
-import random
-
 import requests
+import datetime
+import json
 
 app = Flask(__name__)
 
@@ -16,11 +16,8 @@ def base():
 def home(path):
     return send_from_directory('../frontend/public', path)
 
-
-@app.route("/rand")
-def hello():
-    return str(random.randint(0, 100))
-
+# we need to get someones location and the timeframe in order to filter 
+# down to the produce that is in season for them.
 @app.route("/location")
 def get_country():
 
@@ -28,11 +25,30 @@ def get_country():
     headers_list = request.headers.getlist("X-Forwarded-For")
     user_ip = headers_list[0] if headers_list else request.remote_addr
 
+    # we then query this api to get their state
     try:
+        # here is how we get the state
         response = requests.get("http://ip-api.com/json/{}".format(user_ip))
         js = response.json()
-        country = js['countryCode']
-        return country
+        state = js['regionName']
+
+        # next we get the time of year
+        dt = datetime.datetime.today()
+        if dt.day > 15:
+            period = "late"
+        else:
+            period = "early"
+
+        # finally we are going to create a dictionary
+        diction = {
+            "state": state,
+            "month": dt.month,
+            "period": period
+        }
+
+        # and return it as a json
+        return json.dumps(diction)
+
     except Exception as e:
         return str(e)
 
